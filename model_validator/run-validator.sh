6dc1457c0dd5387bc679c28d574df1d02657fc5e
garydblack <gary.black@pnnl.gov>
2020-09-17 13:03:47 -0700

    setup to run ieee13nodecktassets and main.py instead of direct MV module invocation

--- model_validator/run-validator.sh
+++ model_validator/run-validator.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# SIMREQ={\"power_system_config\":{\"Line_name\":\"_5B816B93-7A5F-B64C-8460-47C17D6E4B0F\"},\"service_configs\":[{\"id\":\"state-estimator\",\"user_options\":{\"use-sensors-for-estimates\":false}}]} # ieee13nodecktassets using simulation
+SIMREQ={\"power_system_config\":{\"Line_name\":\"_5B816B93-7A5F-B64C-8460-47C17D6E4B0F\"},\"service_configs\":[{\"id\":\"state-estimator\",\"user_options\":{\"use-sensors-for-estimates\":false}}]} # ieee13nodecktassets using simulation
 
-# SIMREQ={\"power_system_config\":{\"Line_name\":\"_C1C3E687-6FFD-C753-582B-632A27E28507\"},\"service_configs\":[{\"id\":\"state-estimator\",\"user_options\":{\"use-sensors-for-estimates\":false}}]} # ieee123 using simulation
+#SIMREQ={\"power_system_config\":{\"Line_name\":\"_C1C3E687-6FFD-C753-582B-632A27E28507\"},\"service_configs\":[{\"id\":\"state-estimator\",\"user_options\":{\"use-sensors-for-estimates\":false}}]} # ieee123 using simulation
 
-SIMREQ={\"power_system_config\":{\"Line_name\":\"_AAE94E4A-2465-6F5E-37B1-3E72183A4E44\"},\"service_configs\":[{\"id\":\"state-estimator\",\"user_options\":{\"use-sensors-for-estimates\":false}}]} # test9500new using simulation
+#SIMREQ={\"power_system_config\":{\"Line_name\":\"_AAE94E4A-2465-6F5E-37B1-3E72183A4E44\"},\"service_configs\":[{\"id\":\"state-estimator\",\"user_options\":{\"use-sensors-for-estimates\":false}}]} # test9500new using simulation
 
-SIMREQ={\"power_system_config\":{\"Line_name\":\"_49AD8E07-3BF9-A4E2-CB8F-C3722F837B62\"},\"service_configs\":[{\"id\":\"state-estimator\",\"user_options\":{\"use-sensors-for-estimates\":false}}]} # ieee13nodeckt using simulation
+#SIMREQ={\"power_system_config\":{\"Line_name\":\"_49AD8E07-3BF9-A4E2-CB8F-C3722F837B62\"},\"service_configs\":[{\"id\":\"state-estimator\",\"user_options\":{\"use-sensors-for-estimates\":false}}]} # ieee13nodeckt using simulation
 
-# ./main.py $SIMREQ $1 2>&1 | tee validator.dbg
-python3 transformer_capacity/transformer_capacity.py --request $SIMREQ 2>&1 | tee validator.dbg
-python3 ac_line_ampacity/ac_line_ampacity.py --request $SIMREQ --simid $1 2>&1 | tee validator.dbg
+./main.py $SIMREQ $1 2>&1 | tee validator.dbg
+#python3 transformer_capacity/transformer_capacity.py --request $SIMREQ 2>&1 | tee validator.dbg
+#python3 ac_line_ampacity/ac_line_ampacity.py --request $SIMREQ --simid $1 2>&1 | tee validator.dbg
