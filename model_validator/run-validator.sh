12bdfc61e7462d94badaf6d8a90670b7d9deb60a
garydblack <gary.black@pnnl.gov>
2020-10-23 11:46:58 -0700

    Invoke microservices and then the topology module, which makes a microservice request.  This is an intermediate version because the topology module needs to be integrated with main.py to get fully back to where we were before.

--- model_validator/run-validator.sh
+++ model_validator/run-validator.sh
@@ -4,7 +4,7 @@
 
 #SIMREQ={\"power_system_config\":{\"Line_name\":\"_5B816B93-7A5F-B64C-8460-47C17D6E4B0F\"},\"service_configs\":[{\"id\":\"state-estimator\",\"user_options\":{\"use-sensors-for-estimates\":false}}]} # ieee13nodecktassets using simulation
 
-SIMREQ={\"power_system_config\":{\"Line_name\":\"_C1C3E687-6FFD-C753-582B-632A27E28507\"},\"service_configs\":[{\"id\":\"state-estimator\",\"user_options\":{\"use-sensors-for-estimates\":false}}]} # ieee123 using simulation
+#SIMREQ={\"power_system_config\":{\"Line_name\":\"_C1C3E687-6FFD-C753-582B-632A27E28507\"},\"service_configs\":[{\"id\":\"state-estimator\",\"user_options\":{\"use-sensors-for-estimates\":false}}]} # ieee123 using simulation
 
 #SIMREQ={\"power_system_config\":{\"Line_name\":\"_AAE94E4A-2465-6F5E-37B1-3E72183A4E44\"},\"service_configs\":[{\"id\":\"state-estimator\",\"user_options\":{\"use-sensors-for-estimates\":false}}]} # test9500new using simulation
 
@@ -16,8 +16,20 @@ else
     SIMID=$1
 fi
 
-./main.py "$SIMREQ" $SIMID 2>&1 | tee validator.dbg
+# only start microservices if not already running
+if ! pgrep -f microservices.py -U $USER > /dev/null
+then
+    python3 shared/microservices.py --request "$SIMREQ" --simid "$SIMID" >/tmp/micro.out &
+    # need to wait before issuing any microservices requests
+    sleep 10
+fi
+
+python3 topology/topology_module.py --request "$SIMREQ" 2>&1 | tee validator.log
+#./main.py "$SIMREQ" $SIMID 2>&1 | tee validator.log
+
+# kill microservices so that it starts up with the new simulation next time
+pkill -f microservices.py -U $USER
 
 # standalone invoications of model validation modules
-#python3 transformer_capacity/transformer_capacity.py --request "$SIMREQ" 2>&1 | tee validator.dbg
-#python3 ac_line_ampacity/ac_line_ampacity.py --request "$SIMREQ" --simid $SIMID 2>&1 | tee validator.dbg
+#python3 transformer_capacity/transformer_capacity.py --request "$SIMREQ" 2>&1 | tee validator.log
+#python3 ac_line_ampacity/ac_line_ampacity.py --request "$SIMREQ" --simid $SIMID 2>&1 | tee validator.log
