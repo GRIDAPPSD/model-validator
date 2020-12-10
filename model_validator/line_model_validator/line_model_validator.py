26892a8ea7e14c478cefd6572b495bfb4fbd95dc
garydblack <gary.black@pnnl.gov>
2020-12-09 23:10:37 -0800

    Initial implementation of perLengthSequenceImpedance line validation.

--- model_validator/line_model_validator/line_model_validator.py
+++ model_validator/line_model_validator/line_model_validator.py
@@ -151,11 +151,19 @@ def compareY(line_name, pairA, pairB, YcompValue, Ybus):
 
 
 def check_perLengthPhaseImpedance_lines(sparql_mgr, Ybus):
+    print('\nLINE_MODEL_VALIDATOR perLengthPhaseImpedance validation...', flush=True)
+    print('\nLINE_MODEL_VALIDATOR perLengthPhaseImpedance validation...', file=logfile)
+
     bindings = sparql_mgr.perLengthPhaseImpedance_line_configs()
     #print('LINE_MODEL_VALIDATOR perLengthPhaseImpedance line_configs query results:', flush=True)
     #print(bindings, flush=True)
     #print('LINE_MODEL_VALIDATOR perLengthPhaseImpedance line_configs query results:', file=logfile)
     #print(bindings, file=logfile)
+
+    if len(bindings) == 0:
+        print('\nLINE_MODEL_VALIDATOR perLengthPhaseImpedance: NO LINE MATCHES', flush=True)
+        print('\nLINE_MODEL_VALIDATOR perLengthPhaseImpedance: NO LINE MATCHES', file=logfile)
+        return
 
     Zabc = {}
     for obj in bindings:
@@ -165,7 +173,7 @@ def check_perLengthPhaseImpedance_lines(sparql_mgr, Ybus):
         col = int(obj['col']['value'])
         r_ohm_per_m = float(obj['r_ohm_per_m']['value'])
         x_ohm_per_m = float(obj['x_ohm_per_m']['value'])
-        b_S_per_m = float(obj['b_S_per_m']['value'])
+        #b_S_per_m = float(obj['b_S_per_m']['value'])
         #print('line_config: ' + line_config + ', count: ' + str(count) + ', row: ' + str(row) + ', col: ' + str(col) + ', r_ohm_per_m: ' + str(r_ohm_per_m) + ', x_ohm_per_m: ' + str(x_ohm_per_m) + ', b_S_per_m: ' + str(b_S_per_m))
 
         if line_config not in Zabc:
@@ -189,6 +197,11 @@ def check_perLengthPhaseImpedance_lines(sparql_mgr, Ybus):
     #print(bindings, flush=True)
     #print('LINE_MODEL_VALIDATOR perLengthPhaseImpedance line_names query results:', file=logfile)
     #print(bindings, file=logfile)
+
+    if len(bindings) == 0:
+        print('\nLINE_MODEL_VALIDATOR perLengthPhaseImpedance: NO LINE MATCHES', flush=True)
+        print('\nLINE_MODEL_VALIDATOR perLengthPhaseImpedance: NO LINE MATCHES', file=logfile)
+        return
 
     # map line_name query phase values to nodelist indexes
     ybusPhaseIdx = {'A': '.1', 'B': '.2', 'C': '.3', 's1': '.1', 's2': '.2'}
@@ -301,17 +314,114 @@ def check_perLengthPhaseImpedance_lines(sparql_mgr, Ybus):
 
 
 def check_perLengthSequenceImpedance_lines(sparql_mgr, Ybus):
+    print('\nLINE_MODEL_VALIDATOR perLengthSequenceImpedance validation...', flush=True)
+    print('\nLINE_MODEL_VALIDATOR perLengthSequenceImpedance validation...', file=logfile)
+
     bindings = sparql_mgr.perLengthSequenceImpedance_line_configs()
-    print('LINE_MODEL_VALIDATOR perLengthSequenceImpedance line_configs query results:', flush=True)
-    print(bindings, flush=True)
-    print('LINE_MODEL_VALIDATOR perLengthSequenceImpedance line_configs query results:', file=logfile)
-    print(bindings, file=logfile)
+    #print('LINE_MODEL_VALIDATOR perLengthSequenceImpedance line_configs query results:', flush=True)
+    #print(bindings, flush=True)
+    #print('LINE_MODEL_VALIDATOR perLengthSequenceImpedance line_configs query results:', file=logfile)
+    #print(bindings, file=logfile)
+
+    if len(bindings) == 0:
+        print('\nLINE_MODEL_VALIDATOR perLengthSequenceImpedance: NO LINE MATCHES', flush=True)
+        print('\nLINE_MODEL_VALIDATOR perLengthSequenceImpedance: NO LINE MATCHES', file=logfile)
+        return
+
+    Zabc = {}
+    for obj in bindings:
+        line_config = obj['line_config']['value']
+        r1 = float(obj['r1_ohm_per_m']['value'])
+        x1 = float(obj['x1_ohm_per_m']['value'])
+        #b1 = float(obj['b1_S_per_m']['value'])
+        r0 = float(obj['r0_ohm_per_m']['value'])
+        x0 = float(obj['x0_ohm_per_m']['value'])
+        #b0 = float(obj['b0_S_per_m']['value'])
+        #print('line_config: ' + line_config + ', r1: ' + str(r1) + ', x1: ' + str(x1) + ', b1: ' + str(b1) + ', r0: ' + str(r0) + ', x0: ' + str(x0) + ', b0: ' + str(b0))
+
+        Zs = complex((r0 + 2.0*r1)/3.0, (x0 + 2.0*x1)/3.0)
+        Zm = complex((r0 - r1)/3.0, (x0 - x1)/3.0)
+
+        Zabc[line_config] = np.array([(Zs, Zm, Zm), (Zm, Zs, Zm), (Zm, Zm, Zs)], dtype=complex)
+
+    #for line_config in Zabc:
+    #    print('Zabc[' + line_config + ']: ' + str(Zabc[line_config]))
+    #print('')
 
     bindings = sparql_mgr.perLengthSequenceImpedance_line_names()
     print('LINE_MODEL_VALIDATOR perLengthSequenceImpedance line_names query results:', flush=True)
     print(bindings, flush=True)
     print('LINE_MODEL_VALIDATOR perLengthSequenceImpedance line_names query results:', file=logfile)
     print(bindings, file=logfile)
+
+    if len(bindings) == 0:
+        print('\nLINE_MODEL_VALIDATOR perLengthSequenceImpedance: NO LINE MATCHES', flush=True)
+        print('\nLINE_MODEL_VALIDATOR perLengthSequenceImpedance: NO LINE MATCHES', file=logfile)
+        return
+
+    global minPercentDiffReal, maxPercentDiffReal
+    minPercentDiffReal = 100.0
+    maxPercentDiffReal = 0.0
+    global minPercentDiffImag, maxPercentDiffImag
+    minPercentDiffImag = 100.0
+    maxPercentDiffImag = 0.0
+    global greenCountReal, yellowCountReal, redCountReal
+    greenCountReal = yellowCountReal = redCountReal = 0
+    global greenCountImag, yellowCountImag, redCountImag
+    greenCountImag = yellowCountImag = redCountImag = 0
+
+    for obj in bindings:
+        line_name = obj['line_name']['value']
+        bus1 = obj['bus1']['value'].upper()
+        bus2 = obj['bus2']['value'].upper()
+        length = float(obj['length']['value'])
+        line_config = obj['line_config']['value']
+        #print('line_name: ' + line_name + ', line_config: ' + line_config + ', length: ' + str(length) + ', bus1: ' + bus1 + ', bus2: ' + bus2)
+
+        print("\nValidating line_name: " + line_name, flush=True)
+        print("\nValidating line_name: " + line_name, file=logfile)
+
+        # multiply by scalar length
+        lenZabc = Zabc[line_config] * length
+        # invert the matrix
+        invZabc = np.linalg.inv(lenZabc)
+        # test if the inverse * original = identity
+        #identityTest = np.dot(lenZabc, invZabc)
+        #print('identity test for ' + line_name + ': ' + str(identityTest))
+        # negate the matrix and assign it to Ycomp
+        Ycomp = invZabc * -1
+
+        # do comparisons now
+        compareY(line_name, bus1+'.1', bus2+'.1', Ycomp[0,0], Ybus)
+        compareY(line_name, bus1+'.2', bus2+'.1', Ycomp[1,0], Ybus)
+        compareY(line_name, bus1+'.2', bus2+'.2', Ycomp[1,1], Ybus)
+        compareY(line_name, bus1+'.3', bus2+'.1', Ycomp[2,0], Ybus)
+        compareY(line_name, bus1+'.3', bus2+'.2', Ycomp[2,1], Ybus)
+        compareY(line_name, bus1+'.3', bus2+'.3', Ycomp[2,2], Ybus)
+
+    print("\nReal minimum % difference:" + "{:10.6f}".format(minPercentDiffReal), flush=True)
+    print("\nReal minimum % difference:" + "{:10.6f}".format(minPercentDiffReal), file=logfile)
+    print("Real maximum % difference:" + "{:10.6f}".format(maxPercentDiffReal), flush=True)
+    print("Real maximum % difference:" + "{:10.6f}".format(maxPercentDiffReal), file=logfile)
+
+    print("\nReal \u001b[32mGREEN\u001b[37m count:  " + str(greenCountReal), flush=True)
+    print("\nReal GREEN count:  " + str(greenCountReal), file=logfile)
+    print("Real \u001b[33mYELLOW\u001b[37m count: " + str(yellowCountReal), flush=True)
+    print("Real YELLOW count: " + str(yellowCountReal), file=logfile)
+    print("Real \u001b[31mRED\u001b[37m count:    " + str(redCountReal), flush=True)
+    print("Real RED count:    " + str(redCountReal), file=logfile)
+
+    print("\nImag minimum % difference:" + "{:10.6f}".format(minPercentDiffImag), flush=True)
+    print("\nImag minimum % difference:" + "{:10.6f}".format(minPercentDiffImag), file=logfile)
+    print("Imag maximum % difference:" + "{:10.6f}".format(maxPercentDiffImag), flush=True)
+    print("Imag maximum % difference:" + "{:10.6f}".format(maxPercentDiffImag), file=logfile)
+
+    print("\nImag \u001b[32mGREEN\u001b[37m count:  " + str(greenCountImag), flush=True)
+    print("\nImag GREEN count:  " + str(greenCountImag), file=logfile)
+    print("Imag \u001b[33mYELLOW\u001b[37m count: " + str(yellowCountImag), flush=True)
+    print("Imag YELLOW count: " + str(yellowCountImag), file=logfile)
+    print("Imag \u001b[31mRED\u001b[37m count:    " + str(redCountImag), flush=True)
+    print("Imag RED count:    " + str(redCountImag), file=logfile)
 
     return
 
